446aabe48d70e1a2b4f621a7b2f2e435c22b96a2
gunGeongun <kky8586696@naver.com>
2022-05-02 15:20:37 +0900

    poetry commit2

---
+++ Poetry.py
@@ -0,0 +1,36 @@
+import os,sys,time
+
+class Poetry:
+    def __init__(self,title,poet,contents):
+        self.title = title
+        self.poet = poet
+        self.contents = contents
+        self.consoleHeight = os.get_terminal_size().lines
+        self.upSpeed = 0.5
+    def printCLS(self):
+        print('\n'*self.consoleHeight)
+    def printTitle(self):
+        print('\t\t\t' + self.title)
+        time.sleep(self.upSpeed)
+    def printContents(self):
+        for content in self.contents:
+            print('\t' + content)
+            time.sleep(self.upSpeed)
+    def printPoet(self):
+        print('\t\t\t'+self.poet)
+        time.sleep(self.upSpeed)
+    def printBlankLines(self):
+        for i in range(self.consoleHeight-len(self.contents)-7):
+            print()
+            time.sleep(self.upSpeed)
+    def print(self):
+        self.printCLS()
+        self.printTitle()
+        print()
+        time.sleep(self.upSpeed)
+        self.printContents()
+        print()
+        time.sleep(self.upSpeed)
+        self.printContents()
+        self.printBlankLines()
+        input("감상 끝났으면 아무거나 눌러라")
